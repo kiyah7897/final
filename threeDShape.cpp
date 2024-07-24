9e38be4231485c7ea72f56541ae8f86f0c82319e
kiyah7897 <raraj0404@gmail.com>
2024-07-24 09:26:10 -0700

    Update threeDShape.cpp

--- threeDShape.cpp
+++ threeDShape.cpp
@@ -3,10 +3,14 @@
 ThreeDShape::ThreeDShape(){
     numSides = 0;
     volume = 0;
+    maxSides = 100;
+    sides = new sides[maxSides];
 }
-ThreeDShape::ThreeDShape(float v, int n, float* s){
+ThreeDShape::ThreeDShape(float v, int n, float* s,int m){
     numSides = n;
     volume = v;
+    maxSides = m;
+    sides = new sides[maxSides];
     for(int i =0; i < numSides; i++){
         sides[i] = s[i];
     }
@@ -14,6 +18,8 @@ ThreeDShape::ThreeDShape(float v, int n, float* s){
 ThreeDShape::ThreeDShape(const ThreeDShape& rhs){
     numSides = rhs.numSides;
     volume = rhs.volume;
+    maxSides= rhs.maxSides;
+    sides= new sides[maxSides];
     for(int i = 0; i < numSides; i++){
         sides[i] = rhs.sides[i];
     }
@@ -24,6 +30,9 @@ float ThreeDShape::getVolume(){
 }
 int ThreeDShape::getNumSides(){
     return numSides;
+}
+int ThreeDShape::getMaxSides(){
+    return maxSides;
 }
 void ThreeDShape::addSide(float s){
     sides[numSides] = s;
