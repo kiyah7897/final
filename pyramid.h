89b6c17bda45fad765686fda148235159cac8a03
kiyah7897 <raraj0404@gmail.com>
2024-07-24 09:33:58 -0700

    Update pyramid.h

--- pyramid.h
+++ pyramid.h
@@ -1,1 +1,22 @@
+#ifndef PYRAMID_H
+#define PYRAMID_H
 
+#include "threeDShape.h"
+#include <iostream>
+
+class Pyramid : public ThreeDShape{
+int length , width, height;
+public :
+Pyramid();
+Pyramid(const Pyramid& rhs);
+Pyramid(float v, int n, float* s,int m,int l,int w, int h);
+
+int getLength();
+int getWidth();
+int getHeight();
+~Pyramid();
+Pyramid& operator=(const Pyramid& rhs);
+
+void calculateVolume();
+};
+#endif
