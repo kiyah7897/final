24c264b63c77eb1af2a7716e4c5dcf99601917d3
kiyah7897 <raraj0404@gmail.com>
2024-07-24 10:00:56 -0700

    Update pyramid.cpp

--- pyramid.cpp
+++ pyramid.cpp
@@ -1,19 +1,19 @@
 #include "pyramid.h"
 
 Pyramid::Pyramid():ThreeDShape(){\
-  calculateVolume(Pyramid v);
+  calculateVolume();
   length = 0;
   width =0;
   height = 0;
 }
 Pyramid::Pyramid(const Pyramid& rhs):ThreeDShape(rhs){
-  calculateVolume(Pyramid v);
+  calculateVolume();
 length = rhs.length;
   widht = rhs.width;
   height = rhs.height;
 }
 Pyramid::Pyramid(float v, int n ,float* s,int m,float l,float w,float h):ThreeDShape(v,n,s,m){
-  calculateVolume(Pyramid v);
+  calculateVolume();
 length = l;
   width = w;
   height = h;
@@ -30,15 +30,14 @@ float Pyramid::getHeight(){
 Pyramid::~Pyramid(){
 }
 Pyramid & Pyramid:operator=(const Pyramid& rhs){
-  calculateVolume(Pyramid v);
+  calculateVolume();
 length = rhs.length;
   widht = rhs.width;
   height = rhs.height;
   return *this;
 }
-void Pyramid::calculateVolume(Pyramid v);{
-volume = (v.getlength()*v.getWigth() * v.getHeight())/3;
-return volume;
+void Pyramid::calculateVolume();{
+volume = (length*width*height)/3.0;
 }
 
 
